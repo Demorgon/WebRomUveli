96e30624cc2e9fff4ebdca73a0059a91f9dd52ff
Demorgon <Befargon@gmail.com>
2019-08-09 08:01:37 +0300

    Test

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var reviewSwiper = new Swiper ('.swiper-container--review', {
+    loop: false,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+  var employeeSwiper = new Swiper ('.swiper-container--employee', {
+    loop: false,
+    slidesPerView: "auto",
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
